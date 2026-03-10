07851f1f3a04a8c7cb36f54e46c5919ef701ddb0
nevalhr <phneval@gmail.com>
2026-03-10 14:41:54 +0700

    refactor: reorganize request DTOs by filtering interface, struct and improving structure

--- internal/user/dto/request.go
+++ internal/user/dto/request.go
@@ -10,7 +10,14 @@ type RegisterRequest struct {
 	Email    string `json:"email"     validate:"required,email"`
 	Password string `json:"password"  validate:"required,min=8"`
 }
+type LoginRequest struct {
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password"  validate:"required,min=8"`
+}
 
+type RefreshRequest struct {
+	RefreshToken string `json:"refresh_token" validate:"required"`
+}
 type RefreshTokenRepository interface {
 	Create(token *entity.RefreshToken) error
 	FindByToken(token string) (*entity.RefreshToken, error)
@@ -18,28 +25,16 @@ type RefreshTokenRepository interface {
 	DeleteByUserID(userID uuid.UUID) error
 }
 
-type LoginRequest struct {
-	Email    string `json:"email" validate:"required,email"`
-	Password string `json:"password"  validate:"required,min=8"`
-}
 type UserAuthService interface {
 	Register(req *RegisterRequest) (*entity.User, error)
 	Login(req *LoginRequest) (*LoginResponse, error)
 	//using refresh token, to refresh
 	RefreshToken(token string) (*LoginResponse, error)
 	Logout(token string) error
-
-	//todo : kerjain ini buat refresh token., implementasiin juga.
-	//todo : verify gmail
-
 }
 
 type UserRepository interface {
 	FindByEmail(email string) (*entity.User, error)
 	Create(user *entity.User) error
 	FindByID(id string) (*entity.User, error)
-}
-
-type RefreshRequest struct {
-	RefreshToken string `json:"refresh_token" validate:"required"`
 }
